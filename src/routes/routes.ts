e32b4a04b6b5eb584955997f32898733c6968487
Ella Kuksa <beeella2804@gmail.com>
2023-11-27 12:48:44 +0100

    Task 3: deleted users

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,13 +2,11 @@ import express, { Router } from 'express';
 
 const router: Router = express.Router();
 
-import userRoute from './userRoute';
 import healthCheckRoute from './healthCheckRoute';
 import moviesRoute from './moviesRoute';
 import moviesByGenreRoute from './moviesByGenreRoute';
 import genresRoute from './genresRoute';
 
-router.use(userRoute);
 router.use(healthCheckRoute);
 router.use(moviesRoute);
 router.use(moviesByGenreRoute);
